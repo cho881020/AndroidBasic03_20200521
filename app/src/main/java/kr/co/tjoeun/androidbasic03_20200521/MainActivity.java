900df47b1839800191d3f41ef6f6476c4caf5216
kj_cho <kj_cho@idealidea.co.kr>
2020-05-21 15:36:52 +0900

    데이터바인딩 연결하기

--- app/src/main/java/kr/co/tjoeun/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/androidbasic03_20200521/MainActivity.java
@@ -1,15 +1,20 @@
 package kr.co.tjoeun.androidbasic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.tjoeun.androidbasic03_20200521.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvents();
         setValues();
     }
